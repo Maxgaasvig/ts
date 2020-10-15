a81218d4886ec9ceeeb371249088a5f0a3759b9f
MadsOellgaard <madsoellgaard@hotmail.com>
2020-10-15 12:09:22 +0200

    Tilføjese af oversigt.html og JS

---
+++ js/js.js
@@ -0,0 +1,14 @@
+
+function btnTilmeld(){
+    location.href="tilmeld.html"
+}
+
+
+
+function btnDeltager(){
+    location.href="deltager.html"
+}
+
+function btnOversigt(){
+    location.href="oversigt.html"
+}
